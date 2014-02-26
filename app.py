a686ac552dce6f99e84a5cb455e5c4ea9f9352b2
Hackbright Student <user@mini-002.(none)>
2014-02-25 17:00:57 -0800

    Working on creating user database

--- app.py
+++ app.py
@@ -6,15 +6,34 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get('username'):
+        return "User %s is logged in!" %session['username']
+    else:
+        return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-@app.route("/youshouldprobablychangethisurl")
+    if model.authenticate(username, password):
+        flash("User authenticated")
+        session['username'] = username
+    else:
+        flash("Password incorrect, there may be a ferret stampede in progress!!!")
+
+    return redirect(url_for("index"))
+
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,5 +1,22 @@
+import sqlite3
+
 ADMIN_USER="hackbright"
-ADMIN_PASSWORD=803096023
+ADMIN_PASSWORD=5980025637247534551
+
+DB = None
+CONN = None
 
 def authenticate(username, password):
-    return False
+    query = """SELECT username, password FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+
+    if hash(password) == row[1]:
+        return username
+    else:
+        return None
+
+def connect_to_db():
+    global DB, CONN
+    CONN = sqlite3.connect("thewall.db")
+    DB = CONN.cursor()
