f75b782f77ae6565fd372c5c003f27d062901712
Hackbright Student <user@mini-002.(none)>
2014-02-26 12:26:41 -0800

    Working with wall post functionality

--- app.py
+++ app.py
@@ -6,19 +6,23 @@ app.secret_key = "shhhhthisisasecret"
 
 @app.route("/")
 def index():
-    if session.get('username'):
-        return "User %s is logged in!" %session['username']
+    if session.get('user_id'):
+        return "User %s is logged in!" %session['user_id']
     else:
         return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def process_login():
+    model.connect_to_db()
     username = request.form.get("username")
     password = request.form.get("password")
 
-    if model.authenticate(username, password):
+    user_id = model.authenticate(username, password)
+    print user_id
+
+    if user_id:
         flash("User authenticated")
-        session['username'] = username
+        session['user_id'] = user_id
     else:
         flash("Password incorrect, there may be a ferret stampede in progress!!!")
 
@@ -26,6 +30,7 @@ def process_login():
 
 @app.route("/register")
 def register():
+    model.connect_to_db()
     return render_template("register.html")
 
 @app.route('/logout')
@@ -33,7 +38,25 @@ def logout():
     session.clear()
     return redirect(url_for('index'))
 
+@app.route('/user/<username>')
+def view_user(username):
+    model.connect_to_db()
+    user_id = model.get_user_by_name(username)
+    wall_posts = model.get_wall_posts_for_user(user_id)
 
+    return render_template("wall_posts.html", username = username,
+                                              wall_posts = wall_posts,
+                                              user_id = session.get('user_id'))
+
+@app.route('/user/<username>', methods=["POST"])
+def post_to_wall(username):
+    model.connect_to_db()
+    wall_owner = model.get_user_by_name(username)
+    current_user = session.get('user_id')
+    wall_content = request.form.get("wall_content")
+    model.add_wall_post(current_user, wall_owner, wall_content)
+
+    return redirect(url_for('view_user', username=username))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -6,17 +6,44 @@ ADMIN_PASSWORD=5980025637247534551
 DB = None
 CONN = None
 
-def authenticate(username, password):
-    query = """SELECT username, password FROM users WHERE username = ?"""
-    DB.execute(query, (username,))
-    row = DB.fetchone()
-
-    if hash(password) == row[1]:
-        return username
-    else:
-        return None
-
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("thewall.db")
     DB = CONN.cursor()
+
+def authenticate(username, password):
+    query = """SELECT id, username, password FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+
+    if hash(password) == int(row[2]):
+        return row[0]
+    else:
+        return None
+
+def get_user_by_name(username):
+    query = """SELECT id FROM users WHERE username = ?"""
+    DB.execute(query, (username,))
+    row = DB.fetchone()
+
+    return row[0]
+
+def get_wall_posts_for_user(user_id):
+    query = """SELECT users.username, created_at, content FROM wall_posts JOIN users ON (author_id = users.id) WHERE owner_id = ?"""
+    DB.execute(query, (user_id,))
+    rows = DB.fetchall()
+
+    return rows
+
+def add_wall_post(current_user, wall_owner, wall_content):
+    query = """INSERT INTO wall_posts (owner_id, author_id, created_at, content) VALUES (?, ?, CURRENT_TIMESTAMP, ?)"""
+    DB.execute(query, (wall_owner, current_user, wall_content))
+    CONN.commit()
+
+
+
+def main():
+    connect_to_db()
+
+if __name__ == "__main__":
+    main()
